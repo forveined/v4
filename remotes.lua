becebec6b39258f5c5394398f88a0e9fb4cc5f4d
forveined <120341546+forveined@users.noreply.github.com>
2023-06-30 14:45:42 -0400

    Create remotes.lua

---
+++ remotes.lua
@@ -0,0 +1,43 @@
+local CreateServer = Instance.new("RemoteFunction")
+CreateServer.Name = "CreateServer"
+
+CreateServer.Parent = workspace.Script.MainModule.V4.REMOTES
+
+local GetAdmin = Instance.new("RemoteEvent")
+GetAdmin.Name = "GetAdmin"
+
+GetAdmin.Parent = workspace.Script.MainModule.V4.REMOTES
+local Input = Instance.new("RemoteEvent")
+Input.Name = "Input"
+
+Input.Parent = workspace.Script.MainModule.V4.REMOTES
+
+local Morph = Instance.new("RemoteEvent")
+Morph.Name = "Morph"
+
+Morph.Parent = workspace.Script.MainModule.V4.REMOTES
+
+local NewBot = Instance.new("RemoteFunction")
+NewBot.Name = "NewBot"
+
+NewBot.Parent = workspace.Script.MainModule.V4.REMOTES
+
+local Request = Instance.new("RemoteFunction")
+Request.Name = "Request"
+
+Request.Parent = workspace.Script.MainModule.V4.REMOTES
+
+local Select = Instance.new("RemoteFunction")
+Select.Name = "Select"
+
+Select.Parent = workspace.Script.MainModule.V4.REMOTES
+
+local Sent = Instance.new("RemoteEvent")
+Sent.Name = "Sent"
+
+Sent.Parent = workspace.Script.MainModule.V4.REMOTES
+
+local ServerTeleport = Instance.new("RemoteFunction")
+ServerTeleport.Name = "ServerTeleport"
+
+ServerTeleport.Parent = workspace.Script.MainModule.V4.REMOTES
